7f7a3e3ae8ac04874ffaddf5046664bcfd8441bb
Sébastien GAYA <somniia.99@gmail.com>
2017-11-17 11:02:55 +0100

    Use Rotation Vector Sensor and get current coordinates in GameActivity

--- app/src/main/java/com/sebastiengaya/safebreak/GameActivity.java
+++ app/src/main/java/com/sebastiengaya/safebreak/GameActivity.java
@@ -1,13 +1,75 @@
 package com.sebastiengaya.safebreak;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.TriggerEvent;
+import android.hardware.TriggerEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private SensorEventListener rvSensorListener;
+    private Integer[] password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        password = new Integer[3];
+        password[0] = 40;
+        password[1] = -40;
+        password[2] = 20;
+        final TextView valuesText = findViewById(R.id.valuesText);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        if (mSensor == null) {
+            Log.e("SENSOR_ERROR", "No gyroscope found");
+            finish();
+        }
+
+        rvSensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float[] rotationMatrix = new float[16];
+                SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values);
+
+                // Remap coordinate system
+                float[] remappedRotationMatrix = new float[16];
+                SensorManager.remapCoordinateSystem(rotationMatrix,
+                        SensorManager.AXIS_X,
+                        SensorManager.AXIS_Z,
+                        remappedRotationMatrix);
+
+                // Convert to orientations
+                float[] orientations = new float[3];
+                String[] texts = new String[3];
+                SensorManager.getOrientation(remappedRotationMatrix, orientations);
+
+                for(int i = 0; i < 3; i++) {
+                    orientations[i] = (float)(Math.toDegrees(orientations[i]));
+                    texts[i] = Integer.toString((int) orientations[i]);
+                }
+
+                valuesText.setText(texts[0] + " - " + texts[1] + " - " + texts[2]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        mSensorManager.registerListener(rvSensorListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+
     }
 }
